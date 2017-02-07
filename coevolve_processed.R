924a70e26aa14291a39e251b28230663e8b5971b
Subinoy Biswas <suny.bio@gmail.com>
2017-02-08 00:15:36 +0900

    initial commit

---
+++ coevolve_processed.R
@@ -0,0 +1,11 @@
+setwd("~/suny/RESEARCH_DOC/A_THESIS_WORK/CHAPTER_5_COEVOLV_RESIDUE/eukarya")
+
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyr)
+
+
+fen1_pcna <- read_csv("FEN1_INTER_COEV.csv")
+fen1_pcna
+fen1_splitted <- fen1_pcna %>% separate(AA1, "AA1_algn", "AA1", sep="\\(")
