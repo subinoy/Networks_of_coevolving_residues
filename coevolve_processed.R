017000c04258391461ede03772dada8a4b03c5b7
Subinoy Biswas <suny.bio@gmail.com>
2017-02-10 17:33:47 +0900

    processing and plotting working

--- coevolve_processed.R
+++ coevolve_processed.R
@@ -22,5 +22,15 @@ fen1_splitted <- fen1_splitted %>%
 
 fen1_splitted <- fen1_splitted %>% 
     separate(., AA2, into=c("AA2"), sep="\\)", extra="drop")
-fen1_splitted
+fen1_splitted <- fen1_splitted %>% 
+    mutate(AA1_algn=as.numeric(AA1_algn), AA1=as.numeric(AA1),
+           AA2_algn=as.numeric(AA2_algn), AA2=as.numeric(AA2)
+           )
+fen1_freq <- fen1_splitted %>% 
+    filter(Correlation>=0.8) %>% group_by(AA1) %>% 
+    summarise(freq=n()) %>% mutate(protein="fen1")
+fen1_freq
+
+p <- ggplot(fen1_freq, aes(AA1, y=freq, fill=protein)) + geom_bar(stat="identity")
+p+ labs(fill="Protein")
 ##Sys.getenv("PATH")
